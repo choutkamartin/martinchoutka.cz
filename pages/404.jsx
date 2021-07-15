0ed1dbe4d327535412fc5a39882f1a719676257c
Martin Choutka <martafiixek@gmail.com>
2021-07-15 21:17:16 +0200

    Create 404.jsx

---
+++ pages/404.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Link from "next/link"
+
+export default function NotFound() {
+    return (
+        <>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className="my-10 mx-auto max-w-7xl px-4 sm:my-12 sm:px-6 md:my-16 lg:my-20 lg:px-8 xl:my-28">
+                <h1 className="text-5xl font-extrabold mb-4 text-blue-700">404 Error</h1>
+                <h2 className="text-3xl font-extrabold mb-8">Stránka nenalezena / Page not found</h2>
+                <Link href="/">
+                    <a className="text-blue-700 font-medium">
+                        ← Zpět domů / Back to homepage
+                    </a>
+                </Link>
+            </main>
+        </>
+    )
+}
