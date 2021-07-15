7a056835c3c534466934340a2fcdc84e0b3df32d
Martin Choutka <martafiixek@gmail.com>
2021-07-15 20:31:42 +0200

    Added locale files, changed Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      blue: colors.blue
+    },
     extend: {},
   },
   variants: {
