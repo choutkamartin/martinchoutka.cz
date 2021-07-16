1f2aa95d01d907186aa9fb134114d6c78ec8ccd7
Martin Choutka <martafiixek@gmail.com>
2021-07-16 17:30:12 +0200

    Add API routes

---
+++ pages/api/articles.js
@@ -0,0 +1,27 @@
+
+import dbConnect from "../../utils/dbConnect";
+import Article from "../../models/Article";
+
+export default async function handler(req, res) {
+    await dbConnect();
+    if (req.method === "GET") {
+        const page = req.query.page || 1;
+        const limit = 9;
+        const skip = limit * (page - 1);
+        const articles = await Article.find({}).limit(limit).skip(skip);
+        const count = await Article.countDocuments({});
+        const pages = Math.ceil(count / limit);
+        const obj = {
+            meta: {
+                pagination: {
+                    total: count,
+                    pages: pages,
+                    page: page,
+                    limit: limit,
+                },
+            },
+            articles: articles,
+        };
+        res.json(obj);
+    }
+}
