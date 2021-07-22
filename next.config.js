dda973b810c75e763e93b10c568055e15158beb6
Martin Choutka <martafiixek@gmail.com>
2021-07-22 23:23:10 +0200

    Change config files and packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 module.exports = {
+  env: {
+    BUCKET_NAME: process.env.BUCKET_NAME,
+    REGION: process.env.REGION,
+    WEBSITE_NAME: process.env.WEBSITE_NAME,
+  },
   i18n: {
     defaultLocale: "cs",
     locales: ["cs", "en"],
   },
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "martin-choutka.s3.eu-central-1.amazonaws.com",
+    ],
   },
 };
